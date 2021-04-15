400c52d85a428923f71e0b4f92656147690f3d06
Hassan Syyid <hassansyyid@gmail.com>
2021-04-15 18:39:18 -0400

    remove hotfix

--- tap_procore/streams.py
+++ tap_procore/streams.py
@@ -29,8 +29,6 @@ from singer_sdk.typing import (
     StringType,
 )
 
-from singer.catalog import Catalog
-
 SCHEMAS_DIR = Path(__file__).parent / Path("./schemas")
 
 
@@ -72,9 +70,6 @@ class ProcoreStream(RESTStream):
         """Apply a catalog dict, updating any settings overridden within the catalog."""
         catalog = Catalog.from_dict(catalog_dict)
         catalog_entry: singer.CatalogEntry = catalog.get_stream(self.name)
-        if not catalog_entry:
-            # TODO: Get rid of this. Causes all streams to be synced regardless of catalog
-            return
         self.primary_keys = catalog_entry.key_properties
         self.replication_key = catalog_entry.replication_key
         if catalog_entry.replication_method:
